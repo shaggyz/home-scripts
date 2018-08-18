141185ad577513792b1eac046a01676e9f50118a
Nicolas Palumbo <palnicolas@gmail.com>
2018-08-18 13:05:19 +0200

    Remove some vim plugins, adjust vimrc

--- README.md
+++ README.md
@@ -9,19 +9,6 @@ Configurations/plugins for some unix programs that I use every day.
 
 ## Vim 
 
-For vim there is a script to maintain/update plugins and configurations at `vim/update.sh`. It will backup your current configuration in `$HOME/.vim.backup` before to make any change.
+For vim, there is a script to maintain/update plugins and configurations at `vim/update.sh`. It will backup your current configuration in `$HOME/.vim.backup` before to make any change.
 
-Included plugins:
 
-- Pathogen https://github.com/tpope/vim-pathogen - *Plugin / Runtime manager for vim.*
-- NERDTree https://github.com/scrooloose/nerdtree - *A tree explorer for vim.*
-- CtrlP https://github.com/kien/ctrlp.vim - *Open files with CTRL+P*
-- Syntastic https://github.com/vim-syntastic/syntastic - *Syntax checker*
-- Vim Airline https://github.com/vim-airline/vim-airline - *Status/tabline for vim that's light as air*
-- Vim Instant Markdown https://github.com/suan/vim-instant-markdown - *Vim instant Markdown*
-- Emmet https://github.com/mattn/emmet-vim - *HTML CSS Helper*
-- Table mode https://github.com/dhruvasagar/vim-table-mode - *VIM Table Mode for instant table creation.* 
-- Vim Fugitive https://github.com/tpope/vim-fugitive - *A Git wrapper so awesome, it should be illegal*
-- GitGutter https://github.com/airblade/vim-gitgutter - *A Vim plugin which shows a git diff in the gutter*
-- Vim REST Console https://github.com/diepm/vim-rest-console - *A REST console for Vim.*
-- Auto Pairs https://github.com/jiangmiao/auto-pairs - *Vim plugin, insert or delete brackets, parens, quotes in pair*
--- configs/vim/update.sh
+++ configs/vim/update.sh
@@ -2,6 +2,8 @@
 
 #set -x
 
+VIM_SOURCE_DIR=$(dirname "$0")
+
 VIM_CONFIG_BACKUP="$HOME/.vim.backup"
 VIM_CONFIG_DIR="$HOME/.vim"
 VIM_CONFIG_FILE="$HOME/.vimrc"
@@ -12,6 +14,21 @@ VIM_AUTOLOAD="$VIM_CONFIG_DIR/autoload"
 
 GREEN=$(tput setaf 2)
 NORMAL=$(tput sgr0)
+
+#-------------------------------------------------------------------------------
+# This section is used to update the repository with the current configuration
+#-------------------------------------------------------------------------------
+
+if [ "$1" = "--repository" ]; then
+    echo "Updating home-scripts vimrc with your current .vimrc configuration..."
+    rm "$VIM_SOURCE_DIR/vimrc"
+    cp $VIM_CONFIG_FILE "$VIM_SOURCE_DIR/vimrc"
+    git add $VIM_SOURCE_DIR
+    git commit
+    git push origin master
+    echo "Configuration updated in github!"
+    exit
+fi
 
 #-------------------------------------------------------------------------------
 # Backup configuration, and copy new config files from repository
@@ -35,7 +52,7 @@ rm -Rf $VIM_CONFIG_DIR $VIM_CONFIG_FILE
 
 mkdir -p $VIM_CONFIG_DIR $VIM_COLORS $VIM_BUNDLE $VIM_AUTOLOAD
 mkdir -p "$VIM_AUTOLOAD/airline/themes"
-cp vimrc "$VIM_CONFIG_FILE"
+cp "$VIM_SOURCE_DIR/vimrc" "$VIM_CONFIG_FILE"
 
 #-------------------------------------------------------------------------------
 # Install plugins
@@ -55,38 +72,38 @@ curl -Lo "$VIM_COLORS/molokai.vim" "$GITHUB/tomasr/molokai/master/colors/molokai
 curl -Lo "$VIM_COLORS/afterglow.vim" "$GITHUB/danilo-augusto/vim-afterglow/master/colors/afterglow.vim"
 curl -Lo "$VIM_AUTOLOAD/airline/afterglow.vim" "$GITHUB/danilo-augusto/vim-afterglow/master/autoload/airline/themes/afterglow.vim"
 
-# Ctrl-P bundle
+# Ctrl-P bundle (Search files with CTRL+P)
 git clone --dept=1 "https://github.com/kien/ctrlp.vim" "$VIM_BUNDLE/ctrlp.vim"
 
-# NERDTree
+# NERDTree (Display file tree)
 git clone --dept=1 "https://github.com/scrooloose/nerdtree" "$VIM_BUNDLE/nertdree"
 
 # Syntastic
-git clone --dept=1 "https://github.com/vim-syntastic/syntastic" "$VIM_BUNDLE/syntastic"
+# git clone --dept=1 "https://github.com/vim-syntastic/syntastic" "$VIM_BUNDLE/syntastic"
 
-# Vim Airline
+# Vim Airline (Status bar for vim)
 git clone --dept=1 "https://github.com/vim-airline/vim-airline" "$VIM_BUNDLE/vim-airline"
 
-# Instant markdown
+# Instant markdown (Converts markdown to html)
 git clone --dept=1 "https://github.com/suan/vim-instant-markdown" "$VIM_BUNDLE/vim-instant-markdown"
 
-# Emmet
-git clone --dept=1 "https://github.com/mattn/emmet-vim.git" "$VIM_BUNDLE/emmet-vim"
+# Emmet (HTML, CSS, web development tool)
+# git clone --dept=1 "https://github.com/mattn/emmet-vim.git" "$VIM_BUNDLE/emmet-vim"
 
-# Git fugitive
+# Git fugitive (Utilities for git, not used, but required by git gutter)
 git clone --dept=1 "https://github.com/tpope/vim-fugitive.git" "$VIM_BUNDLE/vim-fugitive"
 
-# Git gutter 
+# Git gutter (Display git changes on vim)
 git clone --dept=1 "http://github.com/airblade/vim-gitgutter.git" "$VIM_BUNDLE/vim-gitgutter"
 
 # Auto pair
-git clone --dept=1 "https://github.com/jiangmiao/auto-pairs" "$VIM_BUNDLE/auto-pairs"
+# git clone --dept=1 "https://github.com/jiangmiao/auto-pairs" "$VIM_BUNDLE/auto-pairs"
 
 # Vim table mode
 git clone --dept=1 "https://github.com/dhruvasagar/vim-table-mode" "$VIM_BUNDLE/vim-table-mode"
 
 # Vim REST client
-git clone --dept=1 "https://github.com/diepm/vim-rest-console" "$VIM_BUNDLE/vim-rest-console"
+# git clone --dept=1 "https://github.com/diepm/vim-rest-console" "$VIM_BUNDLE/vim-rest-console"
 
 printf "\n${GREEN}Vim plugins and confguration updated${NORMAL}\n\n"
 
