7e12410c1496d0c073474caf97f0afdd6b940747
Nicolas Palumbo <n@xinax.net>
2024-03-10 21:03:15 +0100

    Adjust copy and paste on linux

--- .config/nvim/lua/config.lua
+++ .config/nvim/lua/config.lua
@@ -5,12 +5,13 @@ if vim.g.neovide then
         -- Linux
         -- Copy/Paste keybindings
         vim.keymap.set('n', '<A-C-v>', '"+P') -- Paste normal mode
-        vim.keymap.set('i', '<A-C-v>', '<ESC>l"+Pli') -- Paste insert mode
+        vim.keymap.set('v', '<A-C-v>', '"+P') -- Paste visual mode
+        vim.keymap.set('i', '<A-C-v>', '<ESC>l"+Pa') -- Paste insert mode
+        vim.keymap.set('c', '<A-C-v>', '<C-R>+') -- Paste command mode
         vim.keymap.set('v', '<A-C-c>', '"+y') -- Copy
         -- Font
         -- https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip
-        vim.o.guifont = "JetBrainsMonoNL Nerd Font:h10.5"
-        --vim.o.guifont = "Hack Nerd Font Mono:h11"
+        vim.o.guifont = "JetBrainsMonoNL Nerd Font:h11.5"
 
     else
         -- macOS
