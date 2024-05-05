86548470f50ea70263d2788083226baf97eeafd4
Nicolas Palumbo <n@xinax.net>
2024-05-05 03:06:01 +0200

    nvim: add new main config file

---
+++ .config/nvim/init.lua
@@ -0,0 +1,25 @@
+--------------------------------------------------------------------------------
+-- Nico's neovim configuration for Linux and macOS
+--------------------------------------------------------------------------------
+-- The following configuration files are part of the configuration too:
+--------------------------------------------------------------------------------
+
+-- ~/.config/nvim/lua/plugins.lua - Plugin installation
+require 'plugins'
+
+-- ~/.config/nvim/lua/editor.lua - Editor settings
+require 'editor'
+
+-- ~/.config/nvim/lua/plugconfig.lua - Plugin configuration
+require 'plugconfig'
+
+-- ~/.config/nvim/lua/syntax.lua - Custom syntax modifications
+require 'syntax'
+
+-- ~/.config/nvim/lua/neovide.lua - Neovide specific configuration
+if vim.g.neovide then
+    require 'neovide'
+end
+
+-- ~/.config/nvim/lua/keybindings.lua - Custom keybindings
+require 'keybindings'
