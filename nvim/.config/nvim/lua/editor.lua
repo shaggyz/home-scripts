41e69591078107f775db60faacfb2b06004abddb
Nicolás Daniel Palumbo <n@xinax.net>
2026-02-04 21:35:14 +0100

    Remove nvim plugins replaced by native features

--- nvim/.config/nvim/lua/editor.lua
+++ nvim/.config/nvim/lua/editor.lua
@@ -36,6 +36,19 @@ vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
     end
 })
 
+-- Automatically trim trailing whitespace on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function()
+        local save_cursor = vim.fn.getpos(".")
+        vim.cmd([[%s/\s\+$//e]])
+        vim.fn.setpos(".", save_cursor)
+    end,
+})
+
+-- Limit parallel updates to prevent SSL syscall errors on fast hardware
+vim.g.plug_threads = 4
+
 vim.cmd('set encoding=utf-8')
 vim.cmd('set fileencoding=utf-8')
 vim.cmd('filetype plugin indent on')
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -8,50 +8,35 @@ vim.cmd('call plug#begin()')
 
 -- CoC for neovim
 Plug('neoclide/coc.nvim', { branch = 'release' })
--- Icons
-Plug 'ryanoasis/vim-devicons'
--- Close all the buffers
-Plug 'schickling/vim-bufonly'
--- Git signs in buffers
-Plug 'lewis6991/gitsigns.nvim'
--- Automatically remove white spaces
-Plug 'ntpeters/vim-better-whitespace'
--- Max width column
-Plug 'xiyaowong/virtcolumn.nvim'
--- Icons used by FZF
+
+--- UI & Icons
 Plug 'nvim-tree/nvim-web-devicons'
--- File tree
-Plug 'nvim-tree/nvim-tree.lua'
--- Git CLI complement
-Plug 'tpope/vim-fugitive'
--- Adds additional highlight groups, used for syntax highlighting
-Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
--- Lualine
 Plug 'nvim-lualine/lualine.nvim'
--- Improved diff tool
+Plug 'nvim-tree/nvim-tree.lua'
+Plug 'xiyaowong/virtcolumn.nvim'
+
+-- Navigation & Git
+Plug 'lewis6991/gitsigns.nvim'
+Plug 'tpope/vim-fugitive'
 Plug 'sindrets/diffview.nvim'
--- Telescope
 Plug 'nvim-lua/plenary.nvim'
 Plug 'nvim-telescope/telescope.nvim'
--- UUID generator
-Plug 'TrevorS/uuid-nvim'
--- Bind named syntax
-Plug 'egberts/vim-syntax-bind-named'
--- IO library for asynchronous Lua
-Plug 'nvim-neotest/nvim-nio'
--- Load .env variables in nvim session
-Plug 'ellisonleao/dotenv.nvim'
--- Jinja2 syntax
-Plug 'glench/vim-jinja2-syntax'
--- Telescope + COC
 Plug 'fannheyward/telescope-coc.nvim'
 
--- Editor themes
+-- Syntax (The heavy lifter)
+Plug('nvim-treesitter/nvim-treesitter', { ['do'] = ':TSUpdate' })
+
+-- Utilities
+Plug 'schickling/vim-bufonly'
+Plug 'TrevorS/uuid-nvim'
+Plug 'ellisonleao/dotenv.nvim'
+
+-- Themes
 Plug 'sainnhe/edge'
 
 -- Python debugger
 -- Plug 'mfussenegger/nvim-dap'
 -- Plug 'mfussenegger/nvim-dap-python'
 -- Plug 'rcarriga/nvim-dap-ui'
-
+--
 vim.cmd('call plug#end()')
